f0ff83ea11dcca4251f563b0266637ced42d11f7
IsSaudade <yutian.lei@uzh.ch>
2025-02-24 20:21:22 +0100

    Initial commit

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -2,3 +2,12 @@
 // each user has their own slug /[id] (/1, /2, /3, ...) and is displayed using this file
 // try to leverage the component library from antd by utilizing "Card" to display the individual user
 // import { Card } from "antd"; // similar to /app/users/page.tsx
+"use client";
+
+import { useParams } from "next/navigation";
+
+export default function UserPage() {
+  const params = useParams();
+
+  return <div>User ID: {params.id}</div>;
+}
